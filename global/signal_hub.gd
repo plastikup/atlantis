8c65e97486ba997c32550337e08372d1a600908e
YHY <yahya.hallani.email@gmail.com>
2026-01-15 02:57:46 -0500

    submarine camera + signalhub init
    😍😍😍

---
+++ global/signal_hub.gd
@@ -0,0 +1,1 @@
+signal sub_in_bound
--- scripts/clue.gd
+++ scripts/clue.gd
@@ -1,25 +1,45 @@
 extends Area2D
 @onready var progress_bar: ProgressBar = $Control/ProgressBar
+@onready var timer: Timer = $Timer
 
-
+var is_in_bound = false
+var sub_in_bound = false
 var is_held:= false
+var is_done:= false
+const PROGRESS_SPEED := 25.0
 
-func _on_area2d_input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
-		if event.pressed:
-			print("Click started")
-			is_held = true
-		else:
-			print("Click released")
-			is_held = false
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("click") and is_in_bound:
+		is_held = true
+	else:
+		is_held = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if progress_bar.value < 100.0:
-		progress_bar.value += 5.0 * delta
+	if not is_done:
+		if is_held:
+			progress_bar.value += PROGRESS_SPEED * delta
+		else:
+			progress_bar.value = 0
+		if progress_bar.value == progress_bar.max_value:
+			is_done = true
+			timer.start()
+	else:
+		progress_bar.value = 100.0
+		
 	
+func _mouse_enter() -> void:
+	is_in_bound = true
+
+func _mouse_exit() -> void:
+	is_in_bound = false
+
+
+func _on_timer_timeout() -> void:
+	queue_free()
