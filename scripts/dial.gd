9e73db62e4d88a1a8e5e49746b72e914ec3ba152
Oriel <128445171+Alkavraq@users.noreply.github.com>
2026-01-15 12:06:42 -0500

    removed dial default color range

--- scripts/dial.gd
+++ scripts/dial.gd
@@ -15,15 +15,7 @@ const ANGLE_RANGE := PI * 0.61
 
 const EDGE_ANGLE = PI/2 + ANGLE_RANGE
 
-var color_ranges := [{
-	'angle_from': EDGE_ANGLE,
-	'angle_to': -EDGE_ANGLE,
-	'color': Color.WHITE,
-}, {
-	'angle_from': -PI/2 - 0.1,
-	'angle_to': -PI/2 + 0.1,
-	'color': Color.DARK_GREEN,
-}]
+var color_ranges := []
 #var color_ranges := [{
 	#'angle_from': -PI/2 - 0.1,
 	#'angle_to': -PI/2 + 0.1,
