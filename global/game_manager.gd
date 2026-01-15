65240f7def295a6ea66a8f6a38eb80559b7a994c
YHY <yahya.hallani.email@gmail.com>
2026-01-15 13:25:51 -0500

    fish anim

--- global/game_manager.gd
+++ global/game_manager.gd
@@ -1,7 +1,7 @@
 extends Node
 
 const GAME_WRAPPER = preload("uid://dbbvrvjjexwuv")
-const LANDING = preload("uid://b2a5nmp8lmi2b")
+const LANDING = preload("res://scenes/landing.tscn")
 
 signal game
 signal main_menu
