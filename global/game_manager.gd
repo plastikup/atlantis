7d949515719809e93e3ff341966758ce8ee4f663
plastikup <zachk3yata2@gmail.com>
2026-01-15 10:39:12 -0500

    improved implementation of surface/bottom holes

--- global/game_manager.gd
+++ global/game_manager.gd
@@ -1,7 +1,7 @@
 extends Node
 
-const GAME_WRAPPER = preload("uid://dbbvrvjjexwuv")
-const LANDING = preload("uid://b2a5nmp8lmi2b")
+const GAME_WRAPPER = preload("res://scenes/world/game_wrapper.tscn")
+const LANDING = preload("res://scenes/landing.tscn")
 
 func load_game_wrapper() -> void:
 	get_tree().change_scene_to_packed(GAME_WRAPPER)
