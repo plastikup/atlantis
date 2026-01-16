7b4fdbabcb04f94d88a325ed5706a5dc48f2e912
plastikup <zachk3yata2@gmail.com>
2026-01-16 02:25:35 -0500

    added rock parallax background

--- scripts/@tool/hole.gd
+++ scripts/@tool/hole.gd
@@ -1,18 +1,18 @@
 @tool
 extends Node2D
 
-enum HoleTypes { to_bottom, to_surface }
-@export var mode: HoleTypes:
-	set(value):
-		mode = value
-		update_hole_type(value)
-
-func update_hole_type(value: HoleTypes):
-	if not Engine.is_editor_hint(): return
-	if value == HoleTypes.to_bottom:
-		%ColorRect.color = "#00000040"
-	elif value == HoleTypes.to_surface:
-		%ColorRect.color = "#FFFFFF40"
+#enum HoleTypes { to_bottom, to_surface }
+#@export var mode: HoleTypes:
+	#set(value):
+		#mode = value
+		#update_hole_type(value)
+#
+#func update_hole_type(value: HoleTypes):
+	#if not Engine.is_editor_hint(): return
+	#if value == HoleTypes.to_bottom:
+		#%ColorRect.color = "#00000040"
+	#elif value == HoleTypes.to_surface:
+		#%ColorRect.color = "#FFFFFF40"
 
 
 @export var layer: int
--- scripts/tile_map_layer.gd
+++ scripts/tile_map_layer.gd
@@ -8,6 +8,8 @@ func _ready() -> void:
 
 
 func only_apply_current_tilemap_layer_collision() -> void:
+	await get_tree().create_timer(1.0).timeout
+
 	var self_is_current_tilemap = get_parent().get_meta('level') == PlayerInfo.depthLevel+1
 	set_collision_enabled(self_is_current_tilemap)
 	get_parent().visible = self_is_current_tilemap
